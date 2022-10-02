58f00a7207abb2c272a8793d0f63f4328cb590b1
Ankitkumar2012 <92797927+Ankitkumar2012@users.noreply.github.com>
2022-10-02 15:13:10 +0530

    Add files via upload

---
+++ models/product.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Review = require("./review");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  img: {
+    type: String,
+    trim: true,
+    default: "/images/product.jpg",
+  },
+  price: {
+    type: Number,
+    min: 0,
+    default: 0,
+  },
+  desc: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+///mongoose middleWare funtion to delete  all the review on product
+productSchema.post("findOneAndDelete", async function (product) {
+  if (product.reviews.length > 0) {
+    await Review.deleteMany({ _id: { $in: product.reviews } });
+  }
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
